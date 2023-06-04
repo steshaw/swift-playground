b195533ed009537e877126886d4c628e931bc1fc
Steven Shaw <steven@steshaw.org>
2023-06-04 13:57:50 +1000

    map

--- control.swift
+++ control.swift
@@ -1,27 +1,53 @@
 do {
+    print("names-0:")
     let names = ["Fred", "Wilma", "Barney", "Betty"]
     for n in names {
         if n != "Wilma" {
-            print(n)
+            print("  \(n)")
         }
+    }
+    print("names-1:")
+    let names1 : [()] = names.map({ if $0 != "Wilma" { print("  \($0)") } })
+    print(names1)
+    print("names-2:")
+    names.forEach({ if $0 != "Wilma" { print("  \($0)") } })
+}
+
+do {
+    print("optional:")
+    for optName : String? in ["John", nil] {
+      //let optName: String? = "John"
+      let indent = "  "
+      print(indent, "optName = \(String(describing: optName))")
+      print(indent, "opt is nil = ", optName == nil)
+      print(indent, "opt type = ", type(of: optName))
+      print(indent, "map")
+      let r : ()? = optName.map{print(indent, $0)}
+      print(indent, "r =", r as Any)
+      //optName.forEach{print(indent, $0)}
     }
 }
 
 do {
-    let optName: String? = "John"
-    print(optName == nil)
-    print(type(of: optName))
-}
-
-do {
+    print("names:")
     let names = ["John", nil, "Jack", "Jill", nil, "Bill"]
-    print(type(of: names))
+    print("  ", type(of: names))
+    print("  loop:")
     for optName in names {
         if let name = optName {
-            print("Hello, \(name)")
+            print("    Hello, \(name)")
         } else {
-            print("Hello, \(String(describing: optName))")
+            print("    Hello, \(String(describing: optName))")
         }
-        print("Hi \(optName ?? "None")")
+        print("    Hi \(optName ?? "None")")
     }
+    print("  map:")
+    names.map { optName in
+        if let name = optName {
+            return "    Hello, \(name)"
+        } else {
+            return "    Hello, \(String(describing: optName))"
+        }
+    }.forEach{print($0)}
+    names.map { optName in return "    Hi \(optName ?? "None")"}.forEach{print($0)}
 }
