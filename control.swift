803db671e3f953e632146cb5ac192643fdb5e478
Steven Shaw <steven@steshaw.org>
2023-06-04 12:24:32 +1000

    Control

---
+++ control.swift
@@ -0,0 +1,26 @@
+do {
+    let names = ["Fred", "Wilma", "Barney", "Betty"]
+    for n in names {
+        if n != "Wilma" {
+            print(n)
+        }
+    }
+}
+
+do {
+    let optName: String? = "John"
+    print(optName == nil)
+    print(type(of: optName))
+}
+
+do {
+    let names = ["John", nil, "Jack", "Jill", nil, "Bill"]
+    print(type(of: names))
+    for optName in names {
+        if let name = optName {
+            print("Hello, \(name)")
+        } else {
+            print("Hello, \(String(describing: optName))")
+        }
+    }
+}
