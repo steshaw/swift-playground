8424bb60dbfef7b797ae2022035be7eb3d8803c7
Steven Shaw <steven@steshaw.org>
2023-06-04 12:11:40 +1000

    Strings

---
+++ strings.swift
@@ -0,0 +1,14 @@
+let first_name = "Fred"
+let last_name = "Flintstone"
+let msg = "Hello, \(first_name) \(last_name)"
+print(msg)
+print()
+let meaning = 42
+print("""
+    The meaning of life, the universe, and everything...
+    Is...
+    Is...
+    Is...
+    \(meaning)
+    """
+)
